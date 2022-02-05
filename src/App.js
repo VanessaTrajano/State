1b7a4d1baee2ab9496f068317649fd7361ef7ea8
VanessaTrajano <vanessapalmeirakelmertrajano@gmail.com>
2022-02-05 16:41:18 -0300

    Subindo a atividade de state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import foto from './moranguinho.jpg'
+
+class State extends Component{
+  state = {
+    nome: "Vanessa",
+    idade: 17,
+    comidaFav: "Estrogonofe",
+    musicas:["I'm Yours", "Fly me to the moon", "I will survive"] 
+  }
+  render(){
+    return(
+      <div>
+        <h1>Meu nome: {this.state.nome}</h1>
+        
+        <h2>Miha idade: {this.state.idade}</h2>
+        
+        <h3>Minha comida favorita: {this.state.comidaFav}</h3>
+
+        <p>Minhas músicas favoritas:</p>
+        <ul>
+          <li>{this.state.musicas[0]}</li>
+          <li>{this.state.musicas[1]}</li>
+          <li>{this.state.musicas[2]}</li>
+        </ul>
+        
+        <p>Minha fruta favorita:</p>
+        <img src={foto} alt="fotinha de morango" />
+      </div>
+    )
+  }
 }
 
-export default App;
+export default State
